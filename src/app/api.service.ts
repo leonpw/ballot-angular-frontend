6c4bdc9785ba2cd211a2aa121c22fb141e392426
eldnctlh <cequeditlabouchedombre@gmail.com>
2022-10-17 20:18:11 +0300

    delegate/vote forms

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,35 +2,49 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http : HttpClient) { }
+  castVote(form: any): Observable<any> {
+    return this.http.post<any>('http://localhost:3000/cast-vote', form);
+  }
 
-  getTotalSupply() : Observable<String> {
+  delegateVotingPower(form: any): Observable<any> {
+    return this.http.post<any>(
+      'http://localhost:3000/delegate-voting-power',
+      form
+    );
+  }
+
+  getTotalSupply(): Observable<String> {
     return this.http.get<String>('http://localhost:3000/get-total-supply');
   }
 
-  listPaymentOrders() : Observable<String> {
+  listPaymentOrders(): Observable<String> {
     return this.http.get<String>('http://localhost:3000/list-payment-orders');
   }
 
-  listVotes() : Observable<String> {
+  listVotes(): Observable<String> {
     return this.http.get<String>('http://localhost:3000/list-votes');
   }
 
-  checkVotingPower() : Observable<String> {
+  checkVotingPower(): Observable<String> {
     return this.http.get<String>('http://localhost:3000/check-voting-power');
   }
 
-  claimPayment() : Observable<String> {
-    return this.http.post<String>('http://localhost:3000/claim-payment', {id: 'id'});
+  claimPayment(): Observable<String> {
+    return this.http.post<String>('http://localhost:3000/claim-payment', {
+      id: 'id',
+    });
   }
 
-  requestVotingToken() : Observable<String> {
-    return this.http.post<String>('http://localhost:3000/request-voting-tokens', {body: 'body'} );
+  requestVotingToken(): Observable<String> {
+    return this.http.post<String>(
+      'http://localhost:3000/request-voting-tokens',
+      { body: 'body' }
+    );
   }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,57 +3,96 @@ import { ethers } from 'ethers';
 import { ApiService } from '../api.service';
 
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-    walletAddress: string;
-    wallet: ethers.Wallet | undefined;
-    etherBalance: string;
-    provider: ethers.providers.BaseProvider;
-    totalSupply: String;
+  walletAddress: string;
+  wallet: ethers.Wallet | undefined;
+  etherBalance: string;
+  provider: ethers.providers.BaseProvider;
+  totalSupply: String;
 
-    privateKey: string;
-    connected: string;
-    apiService: ApiService;
+  privateKey: string;
+  connected: string;
+  proposal: string;
+  amountVote: string;
+  delegateAddress: string;
+  apiService: ApiService;
 
-    constructor(apiService : ApiService) {
-        this.provider = ethers.getDefaultProvider('goerli');
-        this.walletAddress = 'Please login with your private key first';
-        this.etherBalance = "...";
+  constructor(apiService: ApiService) {
+    this.provider = ethers.getDefaultProvider('goerli');
+    this.walletAddress = 'Please login with your private key first';
+    this.etherBalance = '...';
 
-        this.privateKey = '';
-        this.connected = '';
-        
-        this.totalSupply = 'Loading...';
-        this.apiService = apiService;
-    }
+    this.privateKey = '';
+    this.proposal = '';
+    this.amountVote = '';
+    this.delegateAddress = '';
+    this.connected = '';
 
-    ngOnInit(): void {
-        this.apiService.getTotalSupply().subscribe((response) => {
-            this.totalSupply = response;
+    this.totalSupply = 'Loading...';
+    this.apiService = apiService;
+  }
+
+  ngOnInit(): void {
+    this.apiService.getTotalSupply().subscribe((response) => {
+      this.totalSupply = response;
+    });
+  }
+
+  private updateBalance() {
+    this.provider.getBalance(this.walletAddress).then((balanceBN) => {
+      this.etherBalance = ethers.utils.formatEther(balanceBN) + ' ETH';
+    });
+  }
+
+  onKeyUp(event: any) {
+    console.log('Trying to login with: ' + event.target.value);
+    this.privateKey = event.target.value;
+  }
+
+  onChangeDelegateAddress(event: any) {
+    this.delegateAddress = event.target.value;
+  }
+
+  submitDelegate() {
+    if (this.delegateAddress) {
+      this.apiService
+        .delegateVotingPower({
+          to: this.delegateAddress,
+        })
+        .subscribe((response) => {
+          console.log(response);
         });
-
-
-        
     }
+  }
 
-    private updateBalance() {
-        this.provider.getBalance(this.walletAddress).then((balanceBN) => {
-            this.etherBalance = ethers.utils.formatEther(balanceBN) + ' ETH';
+  onChangeProposal(event: any) {
+    this.proposal = event.target.value;
+  }
+
+  onChangeAmountVote(event: any) {
+    this.amountVote = event.target.value;
+  }
+
+  submitVote() {
+    if (this.amountVote && this.proposal) {
+      this.apiService
+        .castVote({
+          proposal: this.proposal,
+          amount: this.amountVote,
+        })
+        .subscribe((response) => {
+          console.log(response);
         });
     }
+  }
 
-    onKeyUp(event: any) {
-        console.log('Trying to login with: ' + event.target.value);
-        this.privateKey = event.target.value;
-    }
-
-    login() {
-        this.wallet = new ethers.Wallet(this.privateKey);
-        this.walletAddress = this.wallet.address;
-        this.updateBalance();
-    }
-
+  login() {
+    this.wallet = new ethers.Wallet(this.privateKey);
+    this.walletAddress = this.wallet.address;
+    this.updateBalance();
+  }
 }
