71d3f72ab34ec257136f408a646c9a8970634bed
Leon <git@wieisleon.nl>
2022-10-17 19:51:04 +0300

    add api.services placeHolders

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,7 +11,26 @@ export class ApiService {
   constructor(private http : HttpClient) { }
 
   getTotalSupply() : Observable<String> {
-    return this.http.get<String>('http://localhost:3000/get-total-supply')
-    
+    return this.http.get<String>('http://localhost:3000/get-total-supply');
+  }
+
+  listPaymentOrders() : Observable<String> {
+    return this.http.get<String>('http://localhost:3000/list-payment-orders');
+  }
+
+  listVotes() : Observable<String> {
+    return this.http.get<String>('http://localhost:3000/list-votes');
+  }
+
+  checkVotingPower() : Observable<String> {
+    return this.http.get<String>('http://localhost:3000/check-voting-power');
+  }
+
+  claimPayment() : Observable<String> {
+    return this.http.post<String>('http://localhost:3000/claim-payment', {id: 'id'});
+  }
+
+  requestVotingToken() : Observable<String> {
+    return this.http.post<String>('http://localhost:3000/request-voting-tokens', {body: 'body'} );
   }
 }
